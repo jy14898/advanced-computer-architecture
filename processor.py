12a9e06c27a79207c97624b72398ec0110b13cfe
Joseph Young <jy14898@my.bristol.ac.yj>
2017-10-22 13:54:51 +0100

    moved to new component system, but idk if its powerful enough

--- processor.py
+++ processor.py
@@ -5,15 +5,26 @@ import functools
 
 '''
 a note someone made here:http://slideplayer.com/slide/9255919/
-
-you need to carry the write register location through the pipeline as its only relavent
-when it leaves the pipeline (well leave as in go back to decode stage)
+https://www.cise.ufl.edu/~mssz/CompOrg/Figure5.6-PipelineControlLines.gif
+'''
 
 '''
+instead of having inputs and outputs marked as constant, i move that decision
+to add_dependency and add a no_wait flag to say dont wait on this input
+
+I could also solve the issue of setup by once all the inputs are connected,
+the component does the first update but doesnt propagate its outputs. This will
+basically init the output values
+
+still doesnt fix it tho because we need an initial signal in the case of PC loop
+'''
+
 class Component(object):
     def __init__(self):
         self.inputs  = {}
         self.outputs = {}
+
+        self.checked_self = False
 
     def name(self):
         return self.name_
@@ -42,16 +53,34 @@ class Component(object):
 
         self.outputs[outkey]["destinations"].append((destination, destination_inkey))
 
+        return self.outputs[outkey]["constant"]
+
     def get_output(self, outkey):
         return self.outputs[outkey]["value"]
 
     def set_output(self, outkey, value):
         self.outputs[outkey]["value"] = value
 
+    # Just to make sure I havent forgotten any input connections as they're important
+    def is_setup(self):
+        if self.checked_self == True:
+            return True
+
+        for inkey in self.inputs:
+            if self.inputs[inkey]["source"] == (None, None):
+                return False
+
+        self.checked_self = True
+        return True
+
     def update_input(self, source, source_outkey, inkey):
+        assert self.is_setup()
+
+        print "Updating input {} @ {} from {} @ {} with value {}".format(inkey, self.name(), source_outkey, source.name(), source.get_output(source_outkey))
+
         assert inkey in self.inputs, "Tried to update inkey {} but doesn't exist".format(inkey)
         assert self.inputs[inkey]["source"] == (source, source_outkey), "Wrong source and outkey"
-        assert self.inputs[inkey]["updated"] != 1, "Input was set twice"
+        assert self.inputs[inkey]["updated"] != 1, "Input {} for {} was set twice".format(inkey, self.name())
         assert self.inputs[inkey]["updated"] != 2, "Attempted to change constant input"
 
         self.inputs[inkey]["updated"] = 1
@@ -60,9 +89,8 @@ class Component(object):
             self.propagate()
 
     def propagate(self):
-        assert all(v["updated"] == 1 or v["updated"] == 2 for v in self.inputs.values())
+        assert self.is_setup()
 
-        # get all outputs of inputs and whack them into dict
         state = {}
 
         for inkey in self.inputs:
@@ -70,16 +98,16 @@ class Component(object):
 
         self.update(state)
 
+        # reset non-constant inputs
+        for inkey in self.inputs:
+            if self.inputs[inkey]["updated"] == 1:
+                self.inputs[inkey]["updated"] = 0
+
         # update non-constant outputs
         for outkey in self.outputs:
             if not self.outputs[outkey]["constant"]:
                 for destination, destination_inkey in self.outputs[outkey]["destinations"]:
                     destination.update_input(self, outkey, destination_inkey)
-
-        # reset non-constant inputs
-        for inkey in self.inputs:
-            if self.inputs[inkey]["updated"] == 1:
-                self.inputs[inkey]["updated"] = 0
 
     def update(self, state):
         raise NotImplementedError('Components must override update()!')
@@ -98,272 +126,177 @@ class Component(object):
         }
 
 class Constant(Component):
-    def __init__(self):
+    def __init__(self, value=0):
         super(Constant, self).__init__()
 
-        self.add_output("out_value", constant=True)
-        self.outputs["out_value"]["value"] = 1337
+        self.add_output("out", constant=True)
+        self.set_output("out", value)
 
 class UserInput(Component):
     def __init__(self):
         super(UserInput, self).__init__()
 
-        self.add_output("out_value")
+        self.add_output("out")
 
     def write(self):
-        num = int(raw_input("Gimme yo digits: "))
-        self.value_ = num
+        self.value_ = raw_input("Value: ")
         self.propagate()
 
     def update(self, state):
-        self.set_output("out_value", self.value_)
+        self.set_output("out", self.value_)
 
 class Latch(Component):
-    def __init__(self):
+    def __init__(self, value, name="latch"):
         super(Latch, self).__init__()
 
-        self.add_input("new_value")
+        self.add_input("in")
         self.add_input("clock")
 
-        self.add_output("out_value")
+        self.add_output("out")
 
-    name_ = "latch"
+        self.name_ = name
+
+        self.set_output("out", value)
 
     def update(self, state):
         if   state["clock"] == 1:
-            self.outputs["out_value"]["value"] = state["new_value"]
+            self.set_output("out", state["in"])
         elif state["clock"] == 0:
             # Keep the output value the same
             pass
 
 class Clock(Component):
-    name_ = "clock"
-
-    def __init__(self):
-        super(Latch, self).__init__()
+    def __init__(self, name="clock"):
+        super(Clock, self).__init__()
 
         self.add_output("ph0")
         self.add_output("ph1")
 
+        self.state_ = 0
+
+        self.name_ = name
+
     def step(self):
-        self.value_ = 1 - self.value_
+        self.state_ = 1 - self.state_
         self.propagate()
 
     def update(self, state):
-        self.set_output("ph0", self.value_)
-        self.set_output("ph1", self.1 - self.value_)
-
-class Clocker:
-    class Phase:
-        def __init__(self):
-            self.clks  = []
-            self.props = []
-
-            def stepper():
-                while True:
-                    for prop in self.props:
-                        prop()
-                    yield
-                    for clk in self.clks:
-                        clk()
-                    yield
-
-            self.stepper = stepper()
-
-        def add_clk(self, clk):
-            self.clks.append(clk)
-
-        def add_prop(self, prop):
-            self.props.append(prop)
-
-    def __init__(self, num_phases):
-        self.num_phases  = num_phases
-        self.phases      = []
-        self.cycle_count = 0
-
-        for i in xrange(num_phases):
-            self.phases.append(Clocker.Phase())
-
-        def stepper():
-            while True:
-                for idx, phase in enumerate(self.phases):
-                    print "Phase {} propagate".format(idx)
-                    phase.stepper.next()
-                    yield
-
-                    print "Phase {} clk".format(idx)
-                    phase.stepper.next()
-                    yield
-
-                self.cycle_count += 1
-
-        self.stepper = stepper()
-
-'''
-Change to new system:
-    All components must now have well defined values every clock - every phase tho?
-    This means a lot of "0"s are gonna be thrown around but it makes
-        things more safe
-'''
-
-class Latch:
-    next_value = None
-
-    def __init__(self, value, name="latch"):
-        self.current_value = value
-
-        self.name = name
-
-    def set_value(self, value):
-        assert value != None, 'Setting {} to None disallowed'.format(self.name)
-        assert self.next_value == None, '{} set twice'.format(self.name)
-
-        print "set {} {}".format(self.name, value)
-
-        self.next_value = value
-
-    def switch(self):
-        assert self.next_value != None, '{} didn\'t recieve a value to switch to'.format(self.name)
-
-        print "switch {} {} -> {}".format(self.name, self.current_value, self.next_value)
-
-        self.current_value = self.next_value
-        self.next_value    = None
-
-    def get_value(self):
-        return self.current_value
-
-'''
-Not sure how well this will work if we multiplex over different phases
-'''
-class Multiplexer:
-    def __init__(self, num_values, output, name="multiplexer"):
-        self.output = output
-        self.values = [None]*num_values
-        self.control = None
-
-        self.name = name
-
-    def set_value(self, index, value):
-        assert value != None, 'Setting {} value to None disallowed'.format(self.name)
-        assert self.values[index] == None, '{} value set twice'.format(self.name)
-
-        print "set {}[{}] = {}".format(self.name, index, value)
-
-        self.values[index] = value
-        self.check_and_send()
-
-    def set_control(self, index):
-        assert index != None, 'Setting {} index to None disallowed'.format(self.name)
-        assert self.control == None, '{} control set twice'.format(self.name)
-
-        print "set {} control = {}".format(self.name, index)
-
-        self.control = index
-        self.check_and_send()
-
-    def check_and_send(self):
-        if all(a is not None for a in self.values) and self.control is not None:
-            self.output(self.values[self.control])
-            self.control = None
-            self.values  = list(None for _ in self.values)
+        self.set_output("ph0", self.state_)
+        self.set_output("ph1", 1 - self.state_)
 
 
-'''
-Change everything?
+class Multiplexer(Component):
+    def __init__(self, num_inputs, name="multiplexer"):
+        super(Multiplexer, self).__init__()
 
-Basically, instead of having a propagate phase, we have a dependency system
-    where it will propagate once all dependencies are clocked?
+        for i in xrange(num_inputs):
+            self.add_input("input_{}".format(i))
 
-    so for eg you tell all your latches to tell you when they've got a new value
-    and then once you've got all your new values, you make your changes
+        self.add_input("control")
+        self.add_output("out")
 
-    any changes after that are considered a new 'lot' of values?
-'''
+        self.name_ = name
 
-def test(instructions):
-    clocker = Clocker(num_phases=2)
+    def update(self, state):
+        self.set_output("out", state["input_{}".format(state["control"])])
 
-    fetched_instruction = Latch(value=instruction.NOOP(), name="fetched instruction latch")
+class Incrementer(Component):
+    def __init__(self, name="Incrementer"):
+        super(Incrementer, self).__init__()
 
+        self.add_input("in")
+        self.add_output("out")
 
-    fetcher = Fetcher(instructions, fetched_instruction)
-    decoder = Decoder(fetched_instruction, None, None, None)
+        self.name_ = name
 
-    '''
-    split PC bit into phase 0, IF into phase 1?
-    pc will have incremented by 1 tho, by the IF part
-
-    these really need to be checked once things getgoing
-    '''
-    clocker.phases[1].add_prop(fetcher.clock)
-    clocker.phases[1].add_prop(decoder.clock)
-
-    '''
-    Doesn't really matter what phase things are on at the moment, but it will later
-    '''
-    # as the later stages arent set up, always select PC + 1
-    clocker.phases[0].add_prop(functools.partial(fetcher.next_PC_mux.set_control, 0))
-    clocker.phases[0].add_prop(functools.partial(fetcher.next_PC_mux.set_value, 1, 1337))
-
-    fetcher.PC.set_value(0)
-    clocker.phases[0].add_clk(fetcher.PC.switch)
-
-    '''
-    I feel like this should be on phase 1 clk?
-    '''
-    # fetched_instruction.set_value(instruction.NOOP())
-    clocker.phases[1].add_clk(fetched_instruction.switch)
+    def update(self, state):
+        self.set_output("out", state["in"] + 1)
 
 
+class Display(Component):
+    def __init__(self, name="Display"):
+        super(Display, self).__init__()
 
-    return clocker.stepper
+        self.add_input("in")
+        self.name_ = name
 
-#class Executer:
-#    def __init__(self,
+    def update(self, state):
+        print state["in"]
 
-'''
-decode instruction to generate control signals for each following stage
-    wb: load or store control signal
-      : whether or not we're writing to register at all
+class Fetcher(Component):
+    def __init__(self, instructions):
+        super(Fetcher, self).__init__()
+        self.name_ = "fetcher"
 
-    mem: whether
+        self.add_input("address")
+        self.add_input("clock")
+        self.add_output("instruction")
 
-pass through branch address
-pass through the register values for operands
-pass through resulting register location
+        self.instructions = instructions
+        self.set_output("instruction", instruction.NOOP())
 
-pass through PC? i dont think i need to tho
+    def update(self, state):
+        if   state["clock"] == 1:
+            self.set_output("instruction", self.instructions[state["address"]])
+        elif state["clock"] == 0:
+            # Keep the output value the same
+            pass
 
+class RegisterFile:
+    def __init__(self, num_registers):
+        super(RegisterFile, self).__init__()
+        self.name_ = "RegisterFile"
 
-write back to register file on the latch stage i think? im not really sure
-that or make a latch which the register file sometimes reads in?
+        self.registers = [0]*num_registers
 
-'''
+        self.add_input("read_sel1")
+        self.add_input("read_sel2")
+
+        self.add_input("write_sel")
+        self.add_input("write_enable")
+        self.add_input("write_data")
+
+        self.add_input("clock")
+
+        self.add_output("read_data1")
+        self.add_output("read_data2")
+
+    def update(self, state):
+        # define clock high to mean write
+        if state["clock"] == 1:
+            if state["write_enable"] == 1:
+                # disallow writing to R0
+                assert state["write_sel"] != 0, "Cannot change R0's value"
+
+                self.registers[state["write_sel"]] = state["write_data"]
+
+        # define clock low to mean read
+        else:
+            self.set_output("read_data1", self.registers[state["read_sel1"]])
+            self.set_output("read_data2", self.registers[state["read_sel2"]])
+
 class Decoder:
-    class RegisterFile:
-        def __init__(self, num_registers):
-            self.registers = [0]*num_registers
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.name_ = "Decoder"
 
-        def __getitem__(self, key):
-            assert isinstance(key,int)
+        self.add_input("instruction")
 
-            return self.registers[key]
+        '''
+        next stage needs to know:
+            ALU operation
+            if its a branch or r->r
 
-        def __setitem__(self, key, value):
-            assert isinstance(key,int)
-            assert key != 0, 'R0 cannot be changed'
+            ALU source - goes into a tmux to decide which to add ie r0 + r1 or r0 + imm
+        '''
 
-            self.registers[key] = value
+        self.add_output("read_sel1")
+        self.add_output("read_sel2")
 
-    # eventually add latches to set registers?
-    def __init__(self, instruction, exec_ctrl, mem_ctrl, wb_ctrl):
-        self.instruction = instruction
-        self.exec_ctrl = exec_ctrl
-        self.mem_ctrl  = mem_ctrl
-        self.wb_ctrl   = wb_ctrl
+        self.add_output("ALU_op")
 
-        self.registerFile = Decoder.RegisterFile(32)
+        self.add_output("WB_enable")
+        self.add_output("WB_sel")
 
     def clock(self):
         instr = self.instruction.get_value()
@@ -418,27 +351,42 @@ class Decoder:
             pass
 
 '''
-https://www.cise.ufl.edu/~mssz/CompOrg/Figure5.6-PipelineControlLines.gif
-basically mux between PC + 1 or some computed PC
-
-need to mux because conditions for some jumps are decided at execute stage
-otherwise stop what doing and redo?
+somehow do a post-setup check to verify everything is connected
 '''
-class Fetcher:
-    def __init__(self, instructions, instruction):
-        self.instructions = instructions
-        self.instruction  = instruction
+def test(instructions):
+    # Components
+    clock             = Clock()
+    PC                = Latch(value=0, name="PC")
+    incPC             = Incrementer()
+    PCmux             = Multiplexer(2, name="PC multiplexer")
+    isbranch          = Constant(0) # 1
+    branchaddr        = Constant(0) # 1337
+    fetcher           = Fetcher(instructions)
+    instruction_latch = Latch(value=instruction.NOOP(), name="instruction latch")
 
-        self.PC = Latch(0, "PC register")
-        self.next_PC_mux = Multiplexer(2, self.PC.set_value, "next PC mux")
+    # Connections
+    PC.add_dependency(clock, "ph1", "clock")
+    PC.add_dependency(PCmux, "out", "in")
 
-    def clock(self):
-        print "Fetching instruction from address {}".format(self.PC.get_value())
+    incPC.add_dependency(PC, "out", "in")
 
-        instruction = self.instructions[self.PC.get_value()]
+    PCmux.add_dependency(incPC,      "out", "input_0")
+    PCmux.add_dependency(branchaddr, "out", "input_1")
+    PCmux.add_dependency(isbranch,   "out", "control")
 
-        print "Fetched instruction {}".format(instruction)
+    fetcher.add_dependency(PC, "out", "address")
+    fetcher.add_dependency(clock, "ph0", "clock")
 
-        self.instruction.set_value(instruction)
+    instruction_latch.add_dependency(fetcher, "instruction", "in")
+    instruction_latch.add_dependency(clock, "ph1", "clock")
 
-        self.next_PC_mux.set_value(0, self.PC.get_value() + 1)
+    # Need to first propagate signals through the system to init
+    # Alternatively we force inputs to be set to changed?
+    incPC.propagate()
+
+    def step():
+        while True:
+            clock.step()
+            yield
+
+    return clock, PC, incPC, PCmux, step()
